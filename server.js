cc21c9f6b2d87ec377dc8bf8c6ef0bb1246a2058
Yukuka85 <124434236+Yukuka85@users.noreply.github.com>
2026-04-09 22:15:34 +0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
+
 
 // In-memory room storage
 const rooms = new Map();
